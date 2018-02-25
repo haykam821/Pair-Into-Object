ebe0e7f8d9ca50c95c9a5816f836a423f06dccb7
haykam821 <haykam821@users.noreply.github.com>
2018-02-25 14:50:12 -0500

    Write some tests

---
+++ test.js
@@ -0,0 +1,43 @@
+const chai = require("chai");
+const assert = chai.assert;
+
+const pair = require("./index.js");
+
+const string = "a string";
+const number = 1337;
+
+const exampleKeys = ["key1", "key2", "key3"];
+const exampleValues = ["value1", "value2", "value3"];
+const exampleOutput = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": "value3",
+}
+
+describe("the function", function() {
+    it("should error if first parameter is a string", function() {
+        assert.throws(function() {
+            pair(string, exampleValues);
+        }, TypeError);
+    });
+    it("should error if first parameter is a number", function () {
+        assert.throws(function () {
+            pair(number, exampleValues);
+        }, TypeError);
+    });
+
+    it("should error if second parameter is a string", function () {
+        assert.throws(function () {
+            pair(exampleKeys, string);
+        }, TypeError);
+    });
+    it("should error if second parameter is a number", function () {
+        assert.throws(function () {
+            pair(exampleKeys, number);
+        }, TypeError);
+    });
+
+    it("should work", function() {
+        assert.strictEqual(exampleOutput, pair(exampleKeys, exampleValues));
+    })
+});
