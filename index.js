5b78b8bff3495a21fc5435248dbadcc82a48dbd1
haykam821 <haykam821@users.noreply.github.com>
2018-04-06 23:35:14 -0400

    ESLint magic!

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function pair(keys, values, fillMissing = true) {
-    const object = {};
+	const object = {};
 
-    if (!Array.isArray(keys)) {
-        throw new TypeError("Keys must be an array");
-    } else if (!Array.isArray(values)) {
-        throw new TypeError("Values must be an array");
-    } else if (keys.length < values.length) {
-        throw new Error("Cannot have more keys than values");
-    } else if (keys.length !== values.length && !fillMissing) {
-        throw new Error("Lengths of keys and values must be the same");
-    }
+	if (!Array.isArray(keys)) {
+		throw new TypeError("Keys must be an array");
+	} else if (!Array.isArray(values)) {
+		throw new TypeError("Values must be an array");
+	} else if (keys.length < values.length) {
+		throw new Error("Cannot have more keys than values");
+	} else if (keys.length !== values.length && !fillMissing) {
+		throw new Error("Lengths of keys and values must be the same");
+	}
 
-    keys.forEach((element, index) => {
-        if (fillMissing) {
-            object[element] = values[index] || null;
-        } else {
-            object[element] = values[index];
-        }
-    });
+	keys.forEach((element, index) => {
+		if (fillMissing) {
+			object[element] = values[index] || null;
+		} else {
+			object[element] = values[index];
+		}
+	});
 
-    return object;
+	return object;
 }
 
 module.exports = pair;
