19d72d1b39a40c3ec056152a2c0872fab5676d01
haykam821 <haykam821@users.noreply.github.com>
2018-05-03 19:01:39 -0400

    Lint the tests

--- test.js
+++ test.js
@@ -11,84 +11,84 @@ const arrayishObject = {0: "hello", 1: "world"};
 const exampleKeys = ["key1", "key2", "key3"];
 const exampleValues = ["value1", "value2", "value3"];
 const exampleOutput = {
-    "key1": "value1",
-    "key2": "value2",
-    "key3": "value3",
-}
+	"key1": "value1",
+	"key2": "value2",
+	"key3": "value3",
+};
 const examplesOutput = pair(exampleKeys, exampleValues);
 
 describe("type errors (first parameter)", function() {
-    it("should error if first parameter is a string", function() {
-        assert.throws(function() {
-            pair(string, exampleValues);
-        }, TypeError);
-    });
-    it("should error if first parameter is a number", function () {
-        assert.throws(function () {
-            pair(number, exampleValues);
-        }, TypeError);
-    });
-    it("should error if first parameter is an object", function () {
-        assert.throws(function () {
-            pair(object, exampleValues);
-        }, TypeError);
-    });
-    it("should error if first parameter is an array-like object", function () {
-        assert.throws(function () {
-            pair(arrayishObject, exampleValues);
-        }, TypeError);
-    });
+	it("should error if first parameter is a string", function() {
+		assert.throws(function() {
+			pair(string, exampleValues);
+		}, TypeError);
+	});
+	it("should error if first parameter is a number", function() {
+		assert.throws(function() {
+			pair(number, exampleValues);
+		}, TypeError);
+	});
+	it("should error if first parameter is an object", function() {
+		assert.throws(function() {
+			pair(object, exampleValues);
+		}, TypeError);
+	});
+	it("should error if first parameter is an array-like object", function() {
+		assert.throws(function() {
+			pair(arrayishObject, exampleValues);
+		}, TypeError);
+	});
 });
 
 describe("type errors (second parameter)", function() {
-    it("should error if second parameter is a string", function () {
-        assert.throws(function () {
-            pair(exampleKeys, string);
-        }, TypeError);
-    });
-    it("should error if second parameter is a number", function () {
-        assert.throws(function () {
-            pair(exampleKeys, number);
-        }, TypeError);
-    });
-    it("should error if second parameter is an object", function () {
-        assert.throws(function () {
-            pair(exampleKeys, object);
-        }, TypeError);
-    });
-    it("should error if second parameter is an array-like object", function () {
-        assert.throws(function () {
-            pair(exampleKeys, arrayishObject);
-        }, TypeError);
-    });
+	it("should error if second parameter is a string", function() {
+		assert.throws(function() {
+			pair(exampleKeys, string);
+		}, TypeError);
+	});
+	it("should error if second parameter is a number", function() {
+		assert.throws(function() {
+			pair(exampleKeys, number);
+		}, TypeError);
+	});
+	it("should error if second parameter is an object", function() {
+		assert.throws(function() {
+			pair(exampleKeys, object);
+		}, TypeError);
+	});
+	it("should error if second parameter is an array-like object", function() {
+		assert.throws(function() {
+			pair(exampleKeys, arrayishObject);
+		}, TypeError);
+	});
 });
 
 describe("input lengths mismatch errors", function() {
-    it("should error if first array is longer than the second and matching w/ null disabled", function () {
-        assert.throws(function () {
-            pair(["this", "array", "is", "longer"], ["than", "this", "one"], false)
-        }, Error);
-    })
-    it("should error if second array is longer than the first", function () {
-        assert.throws(function () {
-            pair(["and", "this", "array"], ["is", "shorter", "than", "this"])
-        }, Error);
-    })
+	it("should error if first array is longer than the second and matching w/ null disabled", function() {
+		assert.throws(function() {
+			pair(["this", "array", "is", "longer"], ["than", "this", "one"], false);
+		}, Error);
+	});
+	it("should error if second array is longer than the first", function() {
+		assert.throws(function() {
+			pair(["and", "this", "array"], ["is", "shorter", "than", "this"]);
+		}, Error);
+	});
 });
 
 describe("output", function() {
-    it("is an object", function() {
-        assert.isObject(examplesOutput, "hello");
-    })
+	it("is an object", function() {
+		assert.isObject(examplesOutput, "hello");
+	});
 
-    it("should map arguments[0][i] to arguments[1][i]", function() {
-        assert.deepEqual(exampleOutput, examplesOutput);
-    })
+	it("should map arguments[0][i] to arguments[1][i]", function() {
+		assert.deepEqual(exampleOutput, examplesOutput);
+	});
 
-    it("keys should be as long as keys input", function () {
-        assert.strictEqual(exampleKeys.length, Object.keys(examplesOutput).length);
-    })
-    it("values should be as long as values input", function () {
-        assert.strictEqual(exampleValues.length, Object.values(examplesOutput).length);
-    })
+	it("keys should be as long as keys input", function() {
+		assert.strictEqual(exampleKeys.length, Object.keys(examplesOutput).length);
+	});
+	it("values should be as long as values input", function() {
+		assert.strictEqual(exampleValues.length, Object.values(examplesOutput).length);
+	});
 });
